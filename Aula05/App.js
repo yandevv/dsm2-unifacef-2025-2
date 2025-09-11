17f9b167571d1cf3cbe9a8fda9ac3dff2f7717b6
YanDevv - Macbook <ybritogomes@gmail.com>
2025-09-11 20:16:17 -0300

    feat(aula05): added in-class implementation

--- Aula05/App.js
+++ Aula05/App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  View,
+  Image,
+  StyleSheet,
+  Button,
+} from "react-native";
+import { FontAwesome, MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={require("./assets/logo.png")} style={styles.imagem} />
+      <Image
+        source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
+        style={{ width: 100, height: 100 }}
+      />
+      <FontAwesome name="home" size={80} color="blue" />
+      <MaterialCommunityIcons name="horse-variant-fast" size={80} color="green" />
+      <Button
+        title="Perfil"
+        onPress={() => alert("Botão Pressionado!")}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: { flex: 1, justifyContent: "center", alignItems: "center" },
+  imagem: { width: 200, height: 200, resizeMode: "contain" },
 });
