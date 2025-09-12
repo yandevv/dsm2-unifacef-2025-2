b64bb70bd7b080211d773148bb36f4615430e82c
YanDevv - Macbook <ybritogomes@gmail.com>
2025-09-12 09:01:34 -0300

    feat(aula05/profile): added profile screen

---
+++ Aula05/screens/ProfileScreen.js
@@ -0,0 +1,30 @@
+import { View, Text, Image, StyleSheet } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function ProfileScreen() {
+  return (
+    <View style={styles.container}>
+      <Image source={require("../assets/avatar.png")} style={styles.avatar} />
+      <Text style={styles.nome}>João Silva</Text>
+      <Ionicons name="settings" size={30} color="gray" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  avatar: {
+    width: 100,
+    height: 100,
+    borderRadius: 50
+  },
+  nome: {
+    fontSize: 22,
+    fontWeight: "bold",
+    marginVertical: 10
+  }
+});
