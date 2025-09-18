8c5b30180b81febc1fdc8bc1d09e7ec52100c262
YanDevv - Macbook <ybritogomes@gmail.com>
2025-09-18 20:08:14 -0300

    fix(prova1/spacedIconsScreen): changed icons to one distinct color each and updated screen print

--- Prova1/screens/SpacedIconsScreen.js
+++ Prova1/screens/SpacedIconsScreen.js
@@ -9,8 +9,8 @@ export default function SpacedIconsScreen() {
   return (
     <View style={styles.container}>
       <Feather name="home" size={64} color="#231c80" />
-      <Feather name="camera" size={64} color="#231c80" />
-      <Feather name="settings" size={64} color="#231c80" />
+      <Feather name="camera" size={64} color="#fcba03" />
+      <Feather name="settings" size={64} color="#801678" />
     </View>
   );
 }
